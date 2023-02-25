353b867ec3a6bac8567c26cbe2003d1c81c08aff
bobnost <europe00@yandex.ru>
2023-02-25 09:46:58 +0300

    Нахождение дистанции между двумя точами

--- src/Platform/Base.java
+++ src/Platform/Base.java
@@ -1,7 +1,13 @@
 package Platform;
 
+import Platform.Position;
+
 /// Здесь будут статичные методы для каких-то базовых вспомогательных вещей
 public class Base
 {
-
+    /// Получить длину между двумя 2д точками в пикселях
+    static double DistanceToPoint(final Position<Integer> iFirst, final Position<Integer> iSecond)
+    {
+       return Math.sqrt(Math.pow(iFirst.GetX() - iSecond.GetX(), 2) + Math.pow(iFirst.GetY() - iSecond.GetY(), 2));
+    }
 }
