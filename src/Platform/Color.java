e400ded0f424001c94491ccb6589b9b31b59be1d
bobnost <europe00@yandex.ru>
2023-02-25 22:26:29 +0300

    Добавли метод задания цвета через 4 байтное значение

--- src/Platform/Color.java
+++ src/Platform/Color.java
@@ -49,4 +49,12 @@ public class Color {
     public void SetAlpha(char iAlpha) {
         _alpha = iAlpha;
     }
+
+    public void SetRGBA(int iColor)
+    {
+        _red = (char)((iColor & 0xff000000) >> 24);
+        _green = (char)((iColor & 0x00ff0000) >> 16);
+        _blue = (char)((iColor & 0x0000ff00) >> 8);
+        _alpha = (char)(iColor & 0x000000ff);
+    }
 }
