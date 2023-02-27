b79c33403390dbbf32253d986e5d2a12c0799016
europka0o <europe00@mail.ru>
2023-02-27 14:03:45 +0300

    Добавил базовый класс для создания копий, возможно нужно будет переделать на интерфейс

---
+++ src/Platform/ICopy.java
@@ -0,0 +1,17 @@
+package Platform;
+
+/// Класс для создания полной копии объекта
+/// Класс является абстрактным и не может быть создан
+public abstract class ICopy
+{
+    public ICopy()
+    {
+
+    }
+
+    /// Собственно, сама метод создания копии
+    public ICopy CreateCopy()
+    {
+        return null;
+    }
+}
